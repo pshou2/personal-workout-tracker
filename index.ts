215c6f856fd8c2d3758749acc0f4f8abcb946831
Phil Shou <philshou@gmail.com>
2026-02-09 15:27:11 -0500

    connect to mongodb

--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
 import app from './src/app';
+import { connectToDB } from './src/config/mongoose'; // Import the mongoose configuration file
 
+
+connectToDB();
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   /* eslint-disable no-console */
